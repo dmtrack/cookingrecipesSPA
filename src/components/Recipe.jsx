4854c49d7b050a34c09ffe0b3e9caba81b525e4c
dmtrack <rightrack888@gmail.com>
2022-10-23 17:13:38 +0600

    add: youtube iframe

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -31,10 +31,40 @@ function Recipe(props) {
             <h6>Category: {recipe.strCategory}</h6>
             {recipe.strArea ? <h6>Area: {recipe.strArea}</h6> : null}
             <p>{recipe.strInstructions}</p>
-            {recipe.strYouTube ? (
+
+            <table className="centered">
+              <thead>
+                <tr>
+                  <th>Ingredients</th>
+                  <th>Measure</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {Object.keys(recipe).map((key) => {
+                  if (key.includes("Ingredient") && recipe[key]) {
+                    return (
+                      <tr key={key}>
+                        <td>{recipe[key]}</td>
+                        <td>{recipe[`strMeasure${key.slice(13)}`]}</td>
+                      </tr>
+                    );
+                  }
+                  return null;
+                })}
+              </tbody>
+            </table>
+
+            {recipe.strYoutube ? (
               <div className="row">
-                <h5>Video recipe</h5>
-                <iframe src={recipe.strYouTube} frameborder="0" />
+                <h5 style={{ margin: "2rem 0 1.5rem" }}>Video recipe</h5>
+                <iframe
+                  title={recipe.strMeal}
+                  src={`https://www.youtube.com/embed/${recipe.strYoutube.slice(
+                    -11
+                  )}`}
+                  allowFullScreen
+                />
               </div>
             ) : null}
 
